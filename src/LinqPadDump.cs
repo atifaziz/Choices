bf160b9e77660594c994899ca6612f14cf482e9f
Atif Aziz <code@raboof.com>
2018-09-18 13:24:39 +0200

    Add LINQPad dump support
    https://www.linqpad.net/CustomizingDump.aspx

---
+++ src/LinqPadDump.cs
@@ -0,0 +1,48 @@
+#region Copyright (c) 2018 Atif Aziz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Choices
+{
+    abstract partial class ChoiceOf1<T>
+    {
+        internal object ToDump() =>
+            Match(a => new { Chosen = 1, Value = (object) a });
+    }
+
+    abstract partial class ChoiceOf2<T1, T2>
+    {
+        internal object ToDump() =>
+            Match(a => new { Chosen = 1, Value = (object) a },
+                  b => new { Chosen = 2, Value = (object) b });
+    }
+
+    abstract partial class ChoiceOf3<T1, T2, T3>
+    {
+        internal object ToDump() =>
+            Match(a => new { Chosen = 1, Value = (object) a },
+                  b => new { Chosen = 2, Value = (object) b },
+                  c => new { Chosen = 3, Value = (object) c });
+    }
+
+    abstract partial class ChoiceOf4<T1, T2, T3, T4>
+    {
+        internal object ToDump() =>
+            Match(a => new { Chosen = 1, Value = (object) a },
+                  b => new { Chosen = 2, Value = (object) b },
+                  c => new { Chosen = 3, Value = (object) c },
+                  d => new { Chosen = 4, Value = (object) d });
+    }
+}
