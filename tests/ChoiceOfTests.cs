0265ada420e6469981813ec69b91d30554ef1396
Atif Aziz <code@raboof.com>
2018-11-14 10:22:29 +0100

    BadChoice shouldn't throw; it's caller should

--- tests/ChoiceOfTests.cs
+++ tests/ChoiceOfTests.cs
@@ -22,6 +22,6 @@ namespace Choices.Tests
     [TestFixture]
     public partial class ChoiceOfTests
     {
-        static Exception BadChoice() => throw new Exception("Invalid choice.");
+        static Exception BadChoice() => new Exception("Invalid choice.");
     }
 }
